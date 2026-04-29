90ed86e24cbbce7380fc2adf1bb20dd52b49eec2
MehtabJaveed <MJaveed1@uclan.ac.uk>
2026-04-29 17:18:39 +0100

    Update 1: amount of turtle
    The program simply asks you how many turtles should race between 2 to 10.
    Any invalid entry would give an invalid message and let you try again until you give the correct input.
    
    Also using the turtle module allows me to create a screen for these Turtles to race in.

---
+++ lib/main.py
@@ -0,0 +1,5 @@
+from races import get_number_of_racers
+
+
+racers = get_number_of_racers()
+print(f"Number of racers: {racers}")
---
+++ lib/races.py
@@ -0,0 +1,14 @@
+
+def get_number_of_racers():
+    racers = 0
+    while True:
+        try:
+            racers = int(input("Enter the number of racers (2-10): "))
+
+            if 2 <= racers <= 10:
+                return racers
+            else:
+                print("Invalid input. Please enter a number between 2 and 10.")
+
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
